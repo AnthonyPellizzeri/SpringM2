864085b2a5dd9b8ae9626c4fd199878ffd501c6e
Anthony pellizzeri <anthony.pellizzeri3@orange.fr>
2021-03-18 23:00:33 +0100

    first commit

---
+++ Clientservice/src/test/java/org/miage/Clientservice/Clientservice/ClientserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miage.Clientservice.Clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
