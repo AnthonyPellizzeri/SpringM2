060e37e0a1ba288f835caceb0bd44a4cd401fe96
Anthony pellizzeri <anthony.pellizzeri3@orange.fr>
2021-03-19 01:36:55 +0100

    add open API

--- Clientservice/src/main/java/org/miage/Clientservice/Clientservice/ClientserviceApplication.java
+++ Clientservice/src/main/java/org/miage/Clientservice/Clientservice/ClientserviceApplication.java
@@ -1,8 +1,11 @@
 package org.miage.Clientservice.Clientservice;
 
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -11,5 +14,11 @@ public class ClientserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientserviceApplication.class, args);
 	}
-
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().info(new Info()
+				.title("Cours API")
+				.version("1.0")
+				.description("Documentation Cours API Miage"));
+	}
 }
